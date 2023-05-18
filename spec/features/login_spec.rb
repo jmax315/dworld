3488462bed3dfd1a8c0f582ecbf6a090818bb73a
John Maxwell <jmax@jmaxhome.com>
2023-05-17 23:10:03 -0400

    GET "/" with a session

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -3,16 +3,28 @@ require "rack/test"
 require "./src/application"
 
 
-context "with no session" do
+describe "/" do
   include Rack::Test::Methods
 
   let(:app) { DworldApplication }
 
-  describe "/" do
-    it "redirects to login" do
-      get "/"
+  context "with no session" do
+    before { get "/" }
 
+    it "redirects to login" do
       expect(last_response.status).to eq(302)
+      expect(last_response.location).to eq("http://example.org/login")
+    end
+  end
+
+  context "with a session" do
+    let(:user_id) { 'steph' }
+
+    before { get "/", {}, {'rack.session' => {user_id: user_id}} }
+
+    it "redirects to their current page" do
+      expect(last_response.status).to eq(302)
+      expect(last_response.location).to eq("http://example.org/view/#{user_id}")
     end
   end
 end
--- src/application.rb
+++ src/application.rb
@@ -1,7 +1,16 @@
 require 'sinatra/base'
 
 class DworldApplication < Sinatra::Application
+  enable :sessions
+
   get '/' do
-    redirect to("/login")
+    rack_session = env['rack.session']
+    user_id = rack_session['user_id']
+
+    if user_id
+      redirect to "/view/#{user_id}"
+    else
+      redirect to("/login")
+    end
   end
 end
